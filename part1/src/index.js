c74e028db8a3fa7c39fd163f060b046d0776deab
MarcoNarvaez <Nefftep79@gmail.com>
2023-01-25 23:37:46 -0300

    componentes

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
+  return React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'p', null, 'hello world, it is ',now.toString()
+    ),
+    React.createElement(
+      'p', null, a, 'plus', b, 'is ', a + b
+    )
+  )
+}
+ReactDOM.render(React.createElement(App, null), 
+document.getElementById('root')) 
